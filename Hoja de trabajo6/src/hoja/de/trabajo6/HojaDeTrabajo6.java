0fa3d7170487dea536316aefb56aee8970641f8e
Jorge <Jorge@192.168.1.2>
2014-08-29 07:38:22 -0600

    creacion del archivo

---
+++ Hoja de trabajo6/src/hoja/de/trabajo6/HojaDeTrabajo6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * hola Irene me contas si lees esto
+ */
+package hoja.de.trabajo6;
+
+/**
+ *
+ * @author Jorge
+ */
+public class HojaDeTrabajo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
